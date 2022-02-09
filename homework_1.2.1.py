af03e9bb56da399d3bea29207eb2fee1593e61bb
AlexeyDyvinets <97021538+AlexeyDyvinets@users.noreply.github.com>
2022-02-09 11:30:27 +0200

    Update homework_1.2.1.py

--- homework_1.2.1.py
+++ homework_1.2.1.py
@@ -11,7 +11,7 @@ def logs_finder(text):
     logs_reader = re.findall(r'eid:(.*)', text)
     log1 = logs_reader[-1].replace(';', '\n').split()
     log2 = logs_reader[-2].replace(';', '\n').split()
-    # print(f'Предпоследние eid:{log2}\nПоследние eid:{log1}')
+    print(f'Предпоследние eid:{log2}\nПоследние eid:{log1}')
     logs_handler(log1,log2)
 
 
