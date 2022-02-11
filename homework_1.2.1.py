eda911aa392609faffef7a1d609c55d6e9fda3c1
AlexeyDyvinets <97021538+AlexeyDyvinets@users.noreply.github.com>
2022-02-11 15:17:05 +0200

    Update homework_1.2.1.py

--- homework_1.2.1.py
+++ homework_1.2.1.py
@@ -1,31 +1,35 @@
 def file_opener(file):
     log = open(file)
-    s_text = log.readlines()
-    # так нет метода клосе снова
-    return s_text
+    text = log.readlines()
+    log.close()
+    return text
 
 
 def logs_finder(s_text, pattern):
-    # что такое s_text, оно ж должно использоваться
-    logs = [x for x in text[::-1] if pattern in x][:2] # обработай кейс если не найдено ни одно совпадение
-    # т.е если список пуст при попытке среза будет ошибка например срез можно делать только если список не пуст например
-    return logs
+    if s_text == 0:
+        return "Ничего не найдено. Список пуст"
+    else:
+        if [x for x in s_text[::-1] if pattern not in x]:
+            return 'Такой строчки в списке нет'
+        else:
+            logs = [x for x in s_text[::-1] if pattern in x][:2]
+            return logs
 
-def logs_handler(logs): # Обращай внимание на подчеркивания ИДЕ
+
+def logs_handler(logs):
     # там где код повторяеться 2 раза выглядит не оч, напиши цикл тогда уже
     log1 = logs[0].strip()
     s1 = log1[log1.find('eid: ') + len('eid: '):].split(';')
     log2 = logs[1].strip()
     s2 = log2[log2.find('eid: ') + len('eid: '):].split(';')
-    log1 = {x.split('.')[0]: x.split('.')[1]for x in s1}
-    log2 = {x.split('.')[0]: x.split('.')[1]for x in s2}
+    log1 = {x.split('.')[0]: x.split('.')[1] for x in s1}
+    log2 = {x.split('.')[0]: x.split('.')[1] for x in s2}
     set1 = set(log1.items())
     set2 = set(log2.items())
     return set1 ^ set2
 
 
 if __name__ == '__main__':
-    text = file_opener('hw.log')
-    logs = logs_finder(text, 'eid: ') # используй полный шаблон лучше  тот что на кувики
-    lgs = logs_handler(logs)
-    print(lgs)
+    open_log = file_opener('hw.log')
+    desired_log = logs_finder(open_log, 'eid: ')
+    print(desired_log)
