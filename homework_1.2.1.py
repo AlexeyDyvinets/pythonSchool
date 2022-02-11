6580643b18f2624dfd0ab19c462863ea48e9e2b2
Marrx1 <marrx1987@gmail.com>
2022-02-11 18:09:24 +0200

    added example

--- homework_1.2.1.py
+++ homework_1.2.1.py
@@ -18,11 +18,16 @@ def logs_handler(logs):
     for log_str in logs:
         log_sep = log_str[log_str.find('eid: ') + len('eid: '):].strip().split(';')
         logs_list.append({x.split('.')[0]: x.split('.')[1] for x in log_sep})
-    return set(logs_list[0].items()) ^ set(logs_list[1].items())
+    return logs_list
+
+
+def check_differences(first_element, second_element):
+    return set(first_element.items()) ^ set(second_element.items())
 
 
 if __name__ == '__main__':
     open_log = file_opener('hw.log')
     desired_log = logs_finder(open_log, 'D:TUpdaterController::SetUniqueParam(429): eid:')
-    print(logs_handler(desired_log))
-
+    last_two_row = logs_handler(desired_log)
+    print(f'Last row: {last_two_row[0]}\nPrevious row: {last_two_row[1]}')
+    print(check_differences(*last_two_row))
