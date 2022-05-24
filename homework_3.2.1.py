d10371fb4f9747991e82f30313524509de8616b0
AlexeyDyvinets <oleksii.dyvinets@nixsolutions.com>
2022-05-24 19:03:59 +0300

    first commit. Errors writing to the log file

--- homework_3.2.1.py
+++ homework_3.2.1.py
@@ -11,11 +11,22 @@ def file_opener(file):
 def error_finder(s_text, pattern):
     error_logs = [x for x in s_text if pattern in x or pattern.capitalize() in x or pattern.upper() in x]
     error_log_as_str = ''.join(error_logs)
-    logging.basicConfig(level='ERROR', filename=' found_errors.log')
+    logging.basicConfig(filename='found_errors.log')
     logger = logging.getLogger()
     logger.error(error_log_as_str)
+    return error_log_as_str
+
+
+def critical_finder(s_text, pattern):
+    error_logs = [x for x in s_text if pattern in x]
+    error_log_as_str = ''.join(error_logs)
+
+
 
 
 if __name__ == '__main__':
     log_file = file_opener('yupdate.log')
     error_finder(log_file, 'error')
+    critical_finder(log_file, 'CRITICAL')
+
+
